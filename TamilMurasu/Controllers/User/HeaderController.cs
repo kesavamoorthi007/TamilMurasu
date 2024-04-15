d187fbde651248b4eca445e3208c13f012dde93e
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2024-04-15 11:52:01 +0530

    Complete

---
+++ TamilMurasu/Controllers/User/HeaderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TamilMurasu.Controllers.User
+{
+    public class HeaderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
